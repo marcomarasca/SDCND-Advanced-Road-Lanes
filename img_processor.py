01458d594dee5991190ee5c2b4ce29490bf42e43
Marco Marasca <marascamarco@gmail.com>
2018-05-08 19:41:51 -0700

    Minor refactoring and experiments

--- img_processor.py
+++ img_processor.py
@@ -11,63 +11,97 @@ class ImageProcessor():
     def __init__(self, calibration_data_file):
 
         # Camera calibration data
+        print("Loading calibration data...")
         calibration_data = cc.load_calibration_data(file_path = calibration_data_file)
         self.mtx = calibration_data['mtx']
         self.dist = calibration_data['dist']
+        print(self.mtx, self.dist)
 
         # Gradient and color thresholding parameters
         self.sobel_kernel = 15
-        self.x_thresh = [20, 100] # Sobel x threshold
-        self.y_thresh = [35, 100] # Sobel y threshold
+        self.x_thresh = [15, 100] # Sobel x threshold
+        self.y_thresh = [30, 100] # Sobel y threshold
+        self.dir_thresh = [0.7, 1.3] # Sobel dir threshold
+
         self.r_thresh = [200, 255] # RGB, Red channel threshold
         self.s_thresh = [100, 255] # HSL, S channel threshold
-        self.l_thresh = [210, 255] # HSL, L channel threshold
+        self.l_thresh = [195, 255] # HSL, L channel threshold
+        self.v_thresh = [170, 255] # HSV, V channel threshold
 
         # Perspective transformation parameters
         # top left, top right = (585, 456), (700, 456)
         # bottom left, bottom right = (297, 658), (1024, 658)
         self.persp_src_left_line = (-0.6989619377, 864.8927335545) # Slope and intercept for left line
         self.persp_src_right_line = (0.6234567901, 19.58024693) # Slope and intercept for right line
-        self.persp_src_top_pct = 0.65 # Percentage from the top
+        self.persp_src_top_pct = 0.655 # Percentage from the top
         self.persp_dst_x_pct = 0.25 # Destination offset percent
 
     def _warp_coordinates(self, img):
 
-        img_shape = img.shape[1::-1]
+        cols = img.shape[1]
+        rows = img.shape[0]
 
-        src_y_offset = img_shape[1] * self.persp_src_top_pct
+        src_y_offset = rows * self.persp_src_top_pct
         left_slope, left_intercept = self.persp_src_left_line
         right_slope, right_intercept = self.persp_src_right_line
-        
+
+        # Top left, Top right, Bottom right, Bottom left        
         src = np.float32([[(src_y_offset - left_intercept) / left_slope, src_y_offset],
                           [(src_y_offset - right_intercept) / right_slope, src_y_offset],
-                          [(img_shape[1] - right_intercept) / right_slope, img_shape[1]],
-                          [(img_shape[1] - left_intercept) / left_slope, img_shape[1]]])
+                          [(rows - right_intercept) / right_slope, rows],
+                          [(rows - left_intercept) / left_slope, rows]])
 
-        dst_x_offset = img_shape[0] * self.persp_dst_x_pct
+        dst_x_offset = cols * self.persp_dst_x_pct
 
         dst = np.float32([[dst_x_offset, 0], 
-                          [img_shape[0] - dst_x_offset, 0], 
-                          [img_shape[0] - dst_x_offset, img_shape[1]],
-                          [dst_x_offset, img_shape[1]]])
+                          [cols - dst_x_offset, 0], 
+                          [cols - dst_x_offset, rows],
+                          [dst_x_offset, rows]])
         
         return src, dst
 
     def _sobel(self, img, orient = 'x', sobel_kernel = 3):
-        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
         # Take the derivative in x or y given orient = 'x' or 'y'
         if orient == 'x':
-            sobel = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
+            sobel = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
         else:
-            sobel = cv2.Sobel(gray_img, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
+            sobel = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
         
         return sobel
 
-    def _apply_thresh(self, ch, thresh = [0, 255]):
-        result = np.zeros_like(ch)
-        result[(ch >= thresh[0]) & (ch <= thresh[1])] = 1
+    def _apply_thresh(self, img, thresh = [0, 255]):
+        result = np.zeros_like(img)
+        result[(img >= thresh[0]) & (img <= thresh[1])] = 1
         return result
+
+    def unwarp_image(self, img):
+
+        img_shape = img.shape[1::-1]
+
+        src, dst = self._warp_coordinates(img)
+        # Given src and dst points, calculate the perspective transform matrix
+        warp_m = cv2.getPerspectiveTransform(dst, src)
+
+        unwarped = cv2.warpPerspective(img, warp_m, img_shape, flags = cv2.INTER_LINEAR)
+
+        return unwarped
+
+    def warp_image(self, img):
+
+        img_shape = img.shape[1::-1]
+
+        src, dst = self._warp_coordinates(img)
+
+        # Given src and dst points, calculate the perspective transform matrix
+        warp_m = cv2.getPerspectiveTransform(src, dst)
+        # Warp the image using OpenCV warpPerspective()
+        warped = cv2.warpPerspective(img, warp_m, img_shape, flags = cv2.INTER_LINEAR)
+
+        return warped
+
+    def undistort_image(self, img):
+
+        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
 
     def sobel_abs_thresh(self, sobel, thresh=[0,255]):
         # Take the absolute value of the derivative or gradient
@@ -99,79 +133,59 @@ class ImageProcessor():
        
         return binary_output
 
+    def gradient_thresh(self, img):
+
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        sobel_x = self._sobel(gray_img, sobel_kernel = self.sobel_kernel, orient = 'x')
+        sobel_y = self._sobel(gray_img, sobel_kernel = self.sobel_kernel, orient = 'y')
+
+        sobel_x_binary = self.sobel_abs_thresh(sobel_x, self.x_thresh)
+        sobel_y_binary = self.sobel_abs_thresh(sobel_y, self.x_thresh)
+
+        sobel_binary = np.zeros_like(sobel_x_binary)
+        sobel_binary[(sobel_x_binary == 1) & (sobel_y_binary == 1)] = 1
+
+        return sobel_binary
+
     def color_thresh(self, img):
+        
+        hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
         r_ch = img[:,:,2]
         r_binary = self._apply_thresh(r_ch, self.r_thresh)
-
-        hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-        s_ch = hls_img[:,:,2]
-        s_binary = self._apply_thresh(s_ch, self.s_thresh)
 
         l_ch = hls_img[:,:,1]
         l_binary = self._apply_thresh(l_ch, self.l_thresh)
 
-        result = np.zeros_like(s_ch)
-        result[(r_binary == 1) & (s_binary == 1) | (l_binary == 1)] = 1
+        s_ch = hls_img[:,:,2]
+        s_binary = self._apply_thresh(s_ch, self.s_thresh)
+
+        v_ch = hsv_img[:,:,2]
+        v_binary = self._apply_thresh(v_ch, self.v_thresh)
+
+        return r_binary, l_binary, s_binary, v_binary
+
+    def threshold_image(self, img):
+
+        g_binary = self.gradient_thresh(img)
+        r_binary, l_binary, s_binary, v_binary = self.color_thresh(img)
+
+        result = np.zeros_like(g_binary)
+        result[((s_binary == 1) & (g_binary == 1)) | ((r_binary == 1) & (s_binary == 1)) | ((v_binary == 1) & (g_binary == 1)) ] = 255
 
         return result
-
-    def warp_image(self, img):
-
-        img_shape = img.shape[1::-1]
-
-        src, dst = self._warp_coordinates(img)
-
-        # Given src and dst points, calculate the perspective transform matrix
-        warp_m = cv2.getPerspectiveTransform(src, dst)
-        # Warp the image using OpenCV warpPerspective()
-        warped = cv2.warpPerspective(img, warp_m, img_shape)
-
-        return warped
-
-    def unwarp_image(self, img):
-
-        img_shape = img.shape[1::-1]
-
-        src, dst = self._warp_coordinates(img)
-        # Given src and dst points, calculate the perspective transform matrix
-        warp_m = cv2.getPerspectiveTransform(dst, src)
-
-        unwarped = cv2.warpPerspective(img, warp_m, img_shape)
-
-        return unwarped
-
-    def undistort_image(self, img):
-
-        undistorted_img = cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
-        
-        return undistorted_img
-
-    def gradient_thresh(self, img):
-
-        sobel_x = self._sobel(img, sobel_kernel = self.sobel_kernel, orient = 'x')
-        sobel_y = self._sobel(img, sobel_kernel = self.sobel_kernel, orient = 'y')
-
-        sobel_x_binary = self.sobel_abs_thresh(sobel_x, self.x_thresh)
-        sobel_y_binary = self.sobel_abs_thresh(sobel_y, self.y_thresh)
-
-        sobel_binary = np.zeros_like(sobel_x_binary)
-        sobel_binary[(sobel_x_binary == 1 ) & (sobel_y_binary == 1)] = 1
-
-        return sobel_binary
 
     def process_image(self, img):
 
         undistorted_img = self.undistort_image(img)
 
-        gradient_binary = self.gradient_thresh(undistorted_img)
-        color_binary = self.color_thresh(undistorted_img)
+        thresholded_image = self.threshold_image(undistorted_img)
 
-        processed_img = np.zeros_like(color_binary)
-        processed_img[(color_binary == 1) | (gradient_binary == 1)] = 255
+        warped_img = self.warp_image(thresholded_image)
 
-        warped_img = self.warp_image(processed_img)
-
-        return undistorted_img, processed_img, warped_img
+        return undistorted_img, thresholded_image, warped_img
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Image processor')
@@ -205,19 +219,14 @@ if __name__ == '__main__':
 
     for img_file in img_files:
         img = cv2.imread(img_file)
-        undistorted_img, processed_img, processed_warped_img = img_processor.process_image(img)
+        undistorted_img, thresholded_img, processed_warped_img = img_processor.process_image(img)
        
         out_file_prefix = os.path.join(args.output, os.path.split(img_file)[1][:-4])
         cv2.imwrite(out_file_prefix + '_processed.jpg', processed_warped_img)
 
         cv2.imwrite(out_file_prefix + '_undistorted.jpg', undistorted_img)
-        cv2.imwrite(out_file_prefix + '_combined.jpg', processed_img)
-
-        sobel_img = img_processor.gradient_thresh(undistorted_img) * 255
-        cv2.imwrite(out_file_prefix + '_sobel.jpg', cv2.cvtColor(sobel_img, cv2.COLOR_GRAY2BGR))
-
-        color_img = img_processor.color_thresh(undistorted_img) * 255
-        cv2.imwrite(out_file_prefix + '_color.jpg', cv2.cvtColor(color_img, cv2.COLOR_GRAY2BGR))
+        cv2.imwrite(out_file_prefix + '_thresholded.jpg', thresholded_img)
+        cv2.imwrite(out_file_prefix + '_gradient.jpg', img_processor.gradient_thresh(undistorted_img) * 255)
 
         src, dst = img_processor._warp_coordinates(img)
 
--- lane_tracker.py
+++ lane_tracker.py
@@ -9,58 +9,76 @@ from collections import deque
 
 class LaneTracker():
 
-    def __init__(self, window_width = 90, window_height = 90, margin = 30, smooth_frames = 15):
+    def __init__(self, window_width = 60, window_height = 90, margin = 40, smooth_frames = 15):
         self.frames = deque(maxlen = smooth_frames)
         self.window_width = window_width
         self.window_height = window_height
         self.margin = margin
 
-    def find_window_centroids(self, img):
-
-        window_centroids = [] # Store the (left,right) window centroid positions per level
-        window = np.ones(self.window_width) # Create our window template that we will use for convolutions
-        
+    def find_lane_start(self, img, window):
         # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
         # and then np.convolve the vertical image slice with the window template 
         
         # Sum quarter bottom of image to get slice, could use a different ratio
-        l_sum = np.sum(img[int(3*img.shape[0]/4):,:int(img.shape[1]/2)], axis=0)
-        l_center = np.argmax(np.convolve(window,l_sum))-self.window_width/2
-        r_sum = np.sum(img[int(3*img.shape[0]/4):,int(img.shape[1]/2):], axis=0)
-        r_center = np.argmax(np.convolve(window,r_sum))-self.window_width/2+int(img.shape[1]/2)
-        
-        # Add what we found for the first layer
-        window_centroids.append((l_center,r_center))
-        
-        # Go through each layer looking for max pixel locations
-        for level in range(1,(int)(img.shape[0]/self.window_height)):
-            # convolve the window into the vertical slice of the image
-            image_layer = np.sum(img[int(img.shape[0]-(level+1)*self.window_height):int(img.shape[0]-level*self.window_height),:], axis=0)
-            conv_signal = np.convolve(window, image_layer)
-            # Find the best left centroid by using past left center as a reference
-            # Use window_width/2 as offset because convolution signal reference is at right side of window, not center of window
-            offset = self.window_width/2
-            l_min_index = int(max(l_center+offset-self.margin,0))
-            l_max_index = int(min(l_center+offset+self.margin,img.shape[1]))
-            l_center = np.argmax(conv_signal[l_min_index:l_max_index])+l_min_index-offset
-            # Find the best right centroid by using past right center as a reference
-            r_min_index = int(max(r_center+offset-self.margin,0))
-            r_max_index = int(min(r_center+offset+self.margin,img.shape[1]))
-            r_center = np.argmax(conv_signal[r_min_index:r_max_index])+r_min_index-offset
-            # Add what we found for that layer
-            window_centroids.append((l_center,r_center))
-        
-        self.frames.append(window_centroids)
+        l_sum = np.sum(img[int(img.shape[0] * .75):, :int(img.shape[1] / 2)], axis=0)
+        l_center = np.argmax(np.convolve(window, l_sum)) - self.window_width/2
+        r_sum = np.sum(img[int(img.shape[0] * .75):, int(img.shape[1] / 2):], axis=0)
+        r_center = np.argmax(np.convolve(window, r_sum)) - self.window_width/2+int(img.shape[1] / 2)
 
-        if not self.frames:
-            return np.array(window_centroids)
+        return l_center, r_center
+
+    def get_window_center(self, img, level, conv_signal, prev_center):
+        offset = self.window_width/2
+        # Find the best centroid by using past center as a reference
+        min_index = int(max(prev_center + offset - self.margin, 0))
+        max_index = int(min(prev_center + offset + self.margin, img.shape[1]))
+        center_max = np.argmax(conv_signal[min_index:max_index])
+
+        # Update the center only if there is some signal
+        if center_max > 0:
+            center = center_max + min_index - offset
         else:
-            return np.average(self.frames, axis = 0)
+            center = prev_center
 
-    def curvature(self, window_centroids, ym_per_pix = 30/720, xm_per_pix = 3.7/700):
+        return center
+
+    def find_lanes_centroids(self, img):
+
+        lane_centroids = [] # Store the (left,right) window centroid positions per level
+        window = np.ones(self.window_width) # Create our window template that we will use for convolutions
+   
+        l_center, r_center = self.find_lane_start(img, window)
+
+        # Add what we found for the first layer
+        lane_centroids.append((l_center,r_center))
+        # Go through each layer looking for max pixel locations
+        for level in range(1, (int)(img.shape[0] / self.window_height)):
+            # Convolve the window into the vertical slice of the image
+            image_layer = np.sum(img[int(img.shape[0] - (level + 1) * self.window_height):
+                                    int(img.shape[0] - level * self.window_height), :], axis=0)
+                                    
+            conv_signal = np.convolve(window, image_layer)
+
+            l_center = self.get_window_center(img, level, conv_signal, l_center)
+            r_center = self.get_window_center(img, level, conv_signal, r_center)
+
+            # Add what we found for that layer
+            lane_centroids.append((l_center,r_center))
         
-        left_fit_x_vals = window_centroids[:,0]
-        right_fit_x_vals = window_centroids[:,1]
+    
+        if not self.frames:
+            lane_centroids = np.array(lane_centroids)
+        else:
+            lane_centroids = np.average(self.frames, axis = 0)
+        
+        self.frames.append(lane_centroids)
+        
+        return lane_centroids
+
+    def curvature(self, lanes_centroids, ym_per_pix = 30/720, xm_per_pix = 3.7/700):
+        
+        left_fit_x_vals = lanes_centroids[:,0]
+        right_fit_x_vals = lanes_centroids[:,1]
 
         fit_y_vals = np.arange(img.shape[0] - (self.window_height / 2), 0, -self.window_height)
 
@@ -74,10 +92,10 @@ class LaneTracker():
 
         return (left_curverad, right_curverad)
 
-    def draw_lanes(self, img, window_centroids, blend = False, marker_width = 20):
+    def draw_lanes(self, img, lanes_centroids, blend = False, marker_width = 20):
 
-        left_fit_x_vals = window_centroids[:,0]
-        right_fit_x_vals = window_centroids[:,1]
+        left_fit_x_vals = lanes_centroids[:,0]
+        right_fit_x_vals = lanes_centroids[:,1]
 
         fit_y_vals = np.arange(img.shape[0] - (self.window_height / 2), 0, -self.window_height)
 
@@ -89,10 +107,10 @@ class LaneTracker():
         left_x_vals = left_fit[0]*y_vals*y_vals + left_fit[1]*y_vals + left_fit[2]
         right_x_vals = right_fit[0]*y_vals*y_vals + right_fit[1]*y_vals + right_fit[2]
 
-        left_lane = list(zip(np.concatenate((left_x_vals - marker_width, left_x_vals[::-1] + marker_width), axis = 0),
-            np.concatenate((y_vals, y_vals[::-1]), axis = 0)))
-        right_lane = list(zip(np.concatenate((right_x_vals - marker_width, right_x_vals[::-1] + marker_width), axis = 0),
-            np.concatenate((y_vals, y_vals[::-1]), axis = 0)))
+        y_vals = np.concatenate((y_vals, y_vals[::-1]), axis = 0)
+
+        left_lane = list(zip(np.concatenate((left_x_vals - marker_width, left_x_vals[::-1] + marker_width), axis = 0), y_vals))
+        right_lane = list(zip(np.concatenate((right_x_vals - marker_width, right_x_vals[::-1] + marker_width), axis = 0), y_vals))
 
         if blend:
             out_img = img
@@ -109,19 +127,19 @@ class LaneTracker():
         output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height),max(0,int(center-width/2)):min(int(center+width/2),img_ref.shape[1])] = 1
         return output
 
-    def draw_windows(self, img, window_centroids, blend = False):
+    def draw_windows(self, img, lanes_centroids, blend = False):
         # Points used to draw all the left and right windows
         l_points = np.zeros_like(img)
         r_points = np.zeros_like(img)
 
         # Go through each level and draw the windows 	
-        for level in range(0, len(window_centroids)):
+        for level in range(0, len(lanes_centroids)):
             # Window_mask is a function to draw window areas
-            l_mask = self.window_mask(self.window_width, self.window_height, img, window_centroids[level][0],level)
-            r_mask = self.window_mask(self.window_width, self.window_height, img, window_centroids[level][1],level)
+            l_mask = self.window_mask(self.window_width, self.window_height, img, lanes_centroids[level][0],level)
+            r_mask = self.window_mask(self.window_width, self.window_height, img, lanes_centroids[level][1],level)
             # Add graphic points from window mask here to total pixels found 
-            l_points[(l_points == 255) | ((l_mask == 1) ) ] = 255
-            r_points[(r_points == 255) | ((r_mask == 1) ) ] = 255
+            l_points[(l_points == 255) | (l_mask == 1) ] = 255
+            r_points[(r_points == 255) | (r_mask == 1) ] = 255
 
         # Draw the results
         template = np.array(r_points + l_points, np.uint8) # add both left and right window pixels together
@@ -134,7 +152,7 @@ class LaneTracker():
         else:
             out_img = template
 
-        out_img = self.draw_lanes(out_img, window_centroids, blend = True, marker_width = 3)
+        out_img = self.draw_lanes(out_img, lanes_centroids, blend = True, marker_width = 3)
 
         return out_img
 
@@ -158,7 +176,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--w_width',
         type=int,
-        default=50,
+        default=60,
         help='Sliding window width'
     )
 
@@ -172,7 +190,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--w_margin',
         type=int,
-        default=30,
+        default=40,
         help='Sliding window margin from center'
     )
 
@@ -196,15 +214,15 @@ if __name__ == '__main__':
         print("Processing image: {}...".format(img_file))
         undistorted_img, _, processed_img = img_processor.process_image(img)
         print('Finding centroids...')
-        window_centroids = lane_tracker.find_window_centroids(processed_img)
-        curvature = lane_tracker.curvature(window_centroids)
+        lanes_centroids = lane_tracker.find_lanes_centroids(processed_img)
+        curvature = lane_tracker.curvature(lanes_centroids)
         print('Curvature: {}'.format(curvature))
-        processed_img = lane_tracker.draw_windows(processed_img, window_centroids, blend = True)
+        processed_img = lane_tracker.draw_windows(processed_img, lanes_centroids, blend = True)
         
         out_file_prefix = os.path.join(args.output, os.path.split(img_file)[1][:-4])
         cv2.imwrite(out_file_prefix + '_windows.jpg', processed_img)
 
-        lanes_img = lane_tracker.draw_lanes(img, window_centroids)
+        lanes_img = lane_tracker.draw_lanes(img, lanes_centroids)
         lanes_img = img_processor.unwarp_image(lanes_img)
 
         undistorted_img = cv2.addWeighted(undistorted_img, 1.0, lanes_img, 1.0, 0)
